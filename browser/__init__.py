500e2e1593808d01fc2c505a1efd19c8d1f60976
mikejmets <mikejmets@900cab63-ccd9-ce39-b999-59782dc24995>
2010-09-06 18:21:49 +0000

    Added venuefolder view with a table

--- browser/__init__.py
+++ browser/__init__.py
@@ -44,6 +44,7 @@ import internaleventview
 import bookingview
 import elfolderview
 import venueview
+import venuefolderview
 
 ##code-section init-module-footer #fill in your manual code here
 ##/code-section init-module-footer
