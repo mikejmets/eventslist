0819d75da9ce7036cf6f89997f441cb290ec78bd
mikejmets <mikejmets@900cab63-ccd9-ce39-b999-59782dc24995>
2010-10-11 13:03:35 +0000

    email rss errors

--- trunk/content/elevent.py
+++ trunk/content/elevent.py
@@ -475,22 +475,28 @@ class ELEvent(BaseFolder, ATEvent, BrowserDefaultMixin):
           if len(self.Description()) > 0:
             txt += "<b>%s</b><br />" % self.Description()
         except UnicodeDecodeError:
-          logging.error('UnicodeError in description of event %s(%s)' % (
-            self.getParentTitle(), self.getId()))
+          msg = 'unicodeerror in description of event %s(%s)' % (
+            self.getparenttitle(), self.getid())
+          self.MailHost.send(msg, 'info@webtide.co.za', 
+              'mike@webtide.co.za', 'Unicode error in content')
         txt += "%s at %s<br />" % (
           self.getStartDate().Date(), 
           self.getStartDate().Time()[:-3])
         try:
           txt += "%s<br />" % self.getVenueName()
         except UnicodeDecodeError:
-          logging.error('UnicodeError in venue of event %s(%s)' % (
-            self.getParentTitle(), self.getId()))
+          msg = 'UnicodeError in venue of event %s(%s)' % (
+            self.getParentTitle(), self.getId())
+          self.MailHost.send(msg, 'info@webtide.co.za', 
+              'mike@webtide.co.za', 'Unicode error in content')
         try:
           if len(self.getText()) > 0:
             txt += "%s" % self.getText()
         except UnicodeDecodeError:
-          logging.error('UnicodeError in body of event %s(%s)' % (
-            self.getParentTitle(), self.getId()))
+          msg = 'UnicodeError in body of event %s(%s)' % (
+            self.getParentTitle(), self.getId())
+          self.MailHost.send(msg, 'info@webtide.co.za', 
+              'mike@webtide.co.za', 'Unicode error in content')
           #Ignore event
         return txt
 
