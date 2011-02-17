1ce86250dddf79cfc255156d54809e59edc29c34
mikejmets <mikejmets@900cab63-ccd9-ce39-b999-59782dc24995>
2011-02-17 12:58:34 +0000

    Add users to upload images

--- trunk/skins/eventslist_templates/getNotAddableTypes.py
+++ trunk/skins/eventslist_templates/getNotAddableTypes.py
@@ -5,7 +5,7 @@ notAddableTypes = ['Favorite']
 if not member or not member.has_role('Manager'):
   user_list = ['ELFolder', 'VenueFolder', 'CalendarXFolder', 
                'Document', 'Event', 'File', 'Link', 'NewsItem',
-               'Folder', 'Image']
+               'Folder']
   notAddableTypes.extend(user_list)
 
 return notAddableTypes
